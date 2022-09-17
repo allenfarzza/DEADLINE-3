df3b83f48f03b2ad4c3573a7833429f5964501a6
rtpgjhd <92903380+addisrhezaa@users.noreply.github.com>
2022-09-17 10:14:57 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+#max length gausah banyak banya
+
 class produk(models.Model):
     idproduk = models.CharField(max_length=10, primary_key=True)
     jumlahstok = models.IntegerField()
